e201331c4d4106e794f44216d705577faf5c91ad
entornosDaw19 <ciclos201819@gmail.com>
2019-01-06 14:00:21 +0100

    Se añade la clase AppPersona

--- pruebaGit/src/pruebaGit/Persona.java
+++ pruebaGit/src/pruebaGit/Persona.java
@@ -1,5 +1,45 @@
 package pruebaGit;
 
-public class Persona {
+public class Persona 
+{
+	private String nombre;
+	private String telefono;
 
+	
+	
+	public String getNombre() 
+	{
+		return nombre;
+	}
+	
+	public void setNombre(String nomb)
+	{
+		nombre = nomb;
+	}
+	
+	public String getTelefono()
+	{
+		return telefono;
+	}
+	
+	public void setTelefono(String tel) 
+	{
+		telefono = tel;
+	}
+	
+	
+	
+	
+	public void vis()
+	{
+		System.out.println("Nombre: "+ nombre +" Telefono: "+telefono);
+	}
+	
+	public String toString ()
+	{
+		return "nombre= "+nombre+" Telefono "+telefono;
+	}
 }
+	
+	
+
